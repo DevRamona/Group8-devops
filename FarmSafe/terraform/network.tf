2a93bf4eac8a11d27ae9226bcf773285cd929525
Nana-Koramah <n.abeasi@alustudent.com>
2025-12-01 11:27:20 +0200

    chore(terraform): add tfsec ignore comment for cloudwatch log group

--- FarmSafe/terraform/network.tf
+++ FarmSafe/terraform/network.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow" {
+  # tfsec:ignore:aws-cloudwatch-log-group-customer-key
   name              = "/aws/vpc/${local.project_name}-${local.environment}"
   retention_in_days = 30
 
